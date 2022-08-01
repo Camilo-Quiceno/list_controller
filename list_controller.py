0bd00bbe43f072b74ff05b174467450f02a5effa
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2022-08-01 13:40:23 -0500

    Adding adittional prints to enhance user experience

--- list_controller.py
+++ list_controller.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def main ():
     print('list_controller.exe was developed in MatCol by @CamiloQuiceno')
     print('************ Welcome to List Controller ************')
+    WEBSITE = "https://m3s.materialise.net"
     operation_time = int(input('Input operation time (in minutes): '))
 
     if not os.path.exists('.\\imgs'):
@@ -20,10 +21,12 @@ def main ():
 
     if not os.path.exists('.\\imgs\\%s' % (today)):
         os.mkdir('.\\imgs\\%s' % (today))
-        print('Folder %s' % (today))
+        print('Folder %s was created' % (today))
+        
+    print('Your images will be stored at folder: %s' % (today))
 
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://m3s.materialise.net")
+    driver.get(WEBSITE)
 
     for i in range(operation_time + 1):
 
@@ -32,12 +35,13 @@ def main ():
         if i % 5 == 0:  
             date = datetime.datetime.now()
             name_image = 'Date %s.%s' % (date.hour, date.minute)
+
             el = driver.find_element(By.TAG_NAME, 'html')
             driver.execute_script("document.body.style.zoom='60%'")
             el.screenshot('.\\imgs\\%s\\%s.png' % (today,name_image))
             driver.refresh()
 
-            print("Say cheese")
+            print("Say cheese, Image: %s" % (name_image))
 
         else:
             driver.execute_script("window.scrollTo(0, 5000);")
@@ -45,11 +49,8 @@ def main ():
             time.sleep(3)
             driver.execute_script("window.scrollTo(0, -5000);")
             time.sleep(2)
-            
-            print("no cheese 2")
     
     driver.close()
-    print('************ Ready ************')
 
 if __name__ == '__main__':
     main()
