a0915d5d2f7af4e6b6d72349bb978760a8e51807
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2022-09-22 13:48:22 -0500

    Improving code lines

--- list_controller.py
+++ list_controller.py
@@ -6,16 +6,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-def main ():
+def capture_function(page_number,driver, today, name_image):
+    driver.execute_script("window.scrollTo(0, 200);")
+    driver.get_screenshot_as_file(f'.\\imgs\\{today}\\{page_number}\\{name_image}.png')
+    print(f"Say Cheese, Image page {page_number}: {name_image}")
+
+def print_welcome():
     print('*************************************************************')
     print('list_controller.exe was developed in MatCol by @Camilo.Quiceno')
     print('*************************************************************')
     print('************** Welcome to List Controller V2.0 **************')
     print('*************************************************************')
     print('*************************************************************')
+
+
+def main ():
+    
+    print_welcome()
     
     WEBSITE = "https://m3s.materialise.net"
     LINK_PAGE_TWO = '//*[@id="maincontent"]/ng-component/div/div[2]/div[2]/div[2]/form/div[2]/pagination/div/ul/li[4]/a'
+    HTML_XPATH = "/html"
 
     operation_time = int(input('Input operation time (in minutes): '))
 
@@ -48,19 +59,13 @@ def main ():
             date = datetime.datetime.now()
             name_image = 'Date %s.%s' % (date.hour, date.minute)
 
-            el = driver.find_element(By.TAG_NAME, 'html')
-            driver.execute_script("window.scrollTo(0, 200);")
-            driver.get_screenshot_as_file('.\\imgs\\%s\\1\\%s.png' % (today,name_image))
-
-            print("Say Cheese, Image: %s" % (name_image))
+            capture_function(1,driver,today,name_image)
 
             try:
                 driver.find_element(By.XPATH, LINK_PAGE_TWO).click()
                 time.sleep(20)
-                el = driver.find_element(By.TAG_NAME, 'html')
-                driver.execute_script("window.scrollTo(0, 200);")
-                driver.get_screenshot_as_file('.\\imgs\\%s\\2\\%s.png' % (today,name_image))
-                print("Say Cheese, Image page 2: %s" % (name_image))
+            
+                capture_function(2,driver,today,name_image)
 
             except:
                 print('Page 2 is not available')
